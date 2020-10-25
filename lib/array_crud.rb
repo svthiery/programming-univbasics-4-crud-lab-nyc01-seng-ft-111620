5dc903243e792999a20d1c5361578117ed5a1f47
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-25 21:21:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Coco"]
-  array << "Max"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
